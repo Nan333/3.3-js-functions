df0cdc818d585452752f521e4b4404c618e4ca03
Nan Chen <nchen333@gmail.com>
2015-05-20 21:58:52 -0400

    1,2,3,5

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function maxOfThree(first, second, third) {
 
 function isVowel(char){
     "use strict";
-    if(char === "a" || "i" || "o" || "e" || "u"){
+    if(char === "a" || "i" || "o" || "e" || "u") {
       return true;
     }
     else {
@@ -61,9 +61,12 @@ function rovarspraket(phrase){
 // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
 // ---------------------
 
-function sum(){
+function sum(arr){
     "use strict";
-    for (var i = 0, sum = 0; i < arr.length; sum += arr[i++]);
+    for (var i = 0, sum = 0;
+      i < arr.length; sum += arr[i++]) {
+        return sum;
+      };
 }
 
 function multiply(){
